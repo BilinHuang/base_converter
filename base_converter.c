24581db342995011c02e43668b68aa1215d6bfcc
BilinHuang <bilinhuang5@gmail.com>
2025-08-10 22:51:26 +1000

    Update base_converter.c

--- base_converter.c
+++ base_converter.c
@@ -1,6 +1,7 @@
 // version 3.2
 // This version supports arbitary argument sequence
 // And supports argument like 10 without "-" when casting a base
+// Comprehensive - to bo completed
 #include <ctype.h>
 #include <malloc.h>
 #include <stdio.h>
@@ -322,3 +323,4 @@ void print_usage(void) {
     printf("s - simplified\n");
     printf("c - comprehensive\n");
 }
+
